cf0c731d33071c5b9d6e64a93635dc5e27cb9c51
Federico Scheu <fscheu@gmail.com>
2022-11-30 23:33:16 -0300

    Fix linter error

--- app/app/tests.py
+++ app/app/tests.py
@@ -7,16 +7,14 @@ from app import calc
 
 
 class CalcTests(SimpleTestCase):
-    """TEst the calc module."""
+    """Test the calc module."""
 
     def test_add_numbers(self):
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
-    
+
     def test_subtract_numbers(self):
         """Test subtracting numbers"""
-        res = calc.subtract(10,15)
-        self.assertEqual(res,5)
-
-      
+        res = calc.subtract(10, 15)
+        self.assertEqual(res, 5)
