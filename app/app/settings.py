03b9935a439496d03593e0e0db5bb173843c9b37
Federico Scheu <fscheu@gmail.com>
2023-01-09 14:11:08 -0300

    Add image recipe API endpoints

--- app/app/settings.py
+++ app/app/settings.py
@@ -144,3 +144,8 @@ AUTH_USER_MODEL = "core.User"
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
+
+# This allows to upload images trough the web browser
+SPECTACULAR_SETTINGS = {
+    "COMPONENT_SPLIT_REQUEST": True,
+}
